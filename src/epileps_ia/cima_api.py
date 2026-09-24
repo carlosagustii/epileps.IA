7966b639b0d5ca570ae8647509ddbb3b74a512d9
carlosagustii <carlosacanga03@gmail.com>
2026-09-24 20:46:42 +0200

    primer intento de hacer el get

--- src/epileps_ia/cima_api.py
+++ src/epileps_ia/cima_api.py
@@ -27,7 +27,46 @@ for i in range(5): #el total de filas es 988 y cada pagina son 200
     pagina+=1
 
 
-print(len(n_registros))
+columnas = []
+for registro in n_registros:
+    url = "https://cima.aemps.es/cima/rest/medicamento"
 
+    params = {
+        "nregistro": registro
+    }
+
+    response = requests.get(url, params=params)
+
+    valores = response.json()
+    fila = {
+        "nregistro": valores.get("nregistro"),
+        "nombre": valores.get("nombre"),
+        "pactivos": valores.get("pactivos"),
+        "labtitular": valores.get("labtitular"),
+        "labcomercializador": valores.get("labcomercializador"),
+        "cn": valores.get("cn"),
+        "dosis": valores.get("dosis"),
+        "forma_farmaceutica_simplificada": valores.get("forma_farmaceutica_simplificada"),
+        "estado_aut": valores.get("estado_aut"),
+        "estado_rev": valores.get("estado_rev"),
+        "vias_administracion": valores.get("vias_administracion"),
+        "comercializado": valores.get("comercializado"),
+        "requiere_receta": valores.get("requiere_receta"),
+        "generico": valores.get("generico"),
+        "afecta_conduccion": valores.get("afecta_conduccion"),
+        "triangulo_negro": valores.get("triangulo_negro"),
+        "medicamento_huerfano": valores.get("medicamento_huerfano"),
+        "biosimilar": valores.get("biosimilar"),
+        "url_html_ficha_tecnica": valores.get("url_html_ficha_tecnica"),
+        "url_foto_materiales": valores.get("url_foto_materiales"),
+        "num_registros_atc": valores.get("num_registros_atc"),
+        "num_principios_activos": valores.get("num_principios_activos"),
+        "num_excipientes": valores.get("num_excipientes")
+    }
+    columnas.append(fila)
+df = pd.DataFrame(columnas) #meto todas las filas añadidas en cada iteración asignada a cada nregistro
+#print(df.head())
+
+df.to_excel("medicamentos_epilepsia.xlsx", index=False) #lo pasamos al excel
 
 
